50e587f69b7256e5441628e4e4cf41438ed3f88e
dailySoftwareProjects <boris.wagner@daily.at>
2022-02-04 15:02:53 +0100

    init project

---
+++ app/src/main/java/at/bwappsandmore/SackerlGackerlApp.kt
@@ -0,0 +1,8 @@
+package at.bwappsandmore
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SackerlGackerlApp : Application() {
+}
---
+++ app/src/main/java/at/bwappsandmore/gackerlsackerlapp/MainActivity.kt
@@ -0,0 +1,8 @@
+package at.bwappsandmore.gackerlsackerlapp
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+}
